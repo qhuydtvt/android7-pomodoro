06546718d667c09b52d9ff4ed051e994ffe25eee
qhuydtvt <qhuydtvt@gmail.com>
2017-01-14 16:35:24 +0700

    Implement user credentials (username, password) saving using sharedpref

--- app/src/main/java/techkids/vn/android7pomodoro/activities/LoginActivity.java
+++ app/src/main/java/techkids/vn/android7pomodoro/activities/LoginActivity.java
@@ -1,15 +1,26 @@
 package techkids.vn.android7pomodoro.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import techkids.vn.android7pomodoro.R;
+import techkids.vn.android7pomodoro.settings.LoginCredentials;
+import techkids.vn.android7pomodoro.settings.SharedPrefs;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     private EditText etUsername;
     private EditText etPassword;
@@ -19,6 +30,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        skipLoginIfPossible();
+
         setContentView(R.layout.activity_login);
 
         etUsername = (EditText) this.findViewById(R.id.et_username);
@@ -32,7 +46,12 @@ public class LoginActivity extends AppCompatActivity {
                 attemptLogin();
             }
         });
+    }
 
+    private void skipLoginIfPossible() {
+        if(SharedPrefs.getInstance().getLoginCredendials() != null) {
+            gotoTaskActivity();
+        }
     }
 
     private void attemptLogin() {
@@ -41,7 +60,15 @@ public class LoginActivity extends AppCompatActivity {
 
         if (username.equals("admin") && password.equals("admin")) {
             // Notifications
+            SharedPrefs.getInstance().put(new LoginCredentials(username, password));
             Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
+            gotoTaskActivity();
         }
     }
+
+    private void gotoTaskActivity() {
+        Intent intent = new Intent(this, TaskActivity.class);
+        startActivity(intent);
+    }
+
 }
